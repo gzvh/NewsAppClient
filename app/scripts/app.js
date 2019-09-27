cab4bd85ffce12750899a1b51d15f51a2d6a343c
Przemysław Cieślarek <madflow12@gmail.com>
2019-09-27 17:47:29 +0200

    1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'RESTService'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -23,6 +24,11 @@ angular
         templateUrl: 'views/main.html',
         controller: 'NewsController',
         controllerAs: 'newsdashboard'
+      })
+      .when('/displaynews/:id', {
+        templateUrl: 'views/display.html',
+        controller: 'NewsDisplayController',
+        controllerAs: 'displaydash'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
--- app/scripts/restservice.js
+++ app/scripts/restservice.js
@@ -1,25 +1,42 @@
 'use strict';
 
+var path = '';
 
 angular.module('RESTService', [])
-  .controller('NewsController', function (NewsModel) {
+  .controller('NewsController', function(NewsModel) {
 
   var newsdashboard = this;
   function getNews() {
+    path = 'getNews/';
     NewsModel.all().then(function(result) {
       newsdashboard.news = result.data;
       console.log(newsdashboard.news);
-    })
+    });
   }
 
   newsdashboard.data = [];
   getNews();
 })
+  .controller('NewsDisplayController', function ($routeParams, NewsModel) {
+
+    var displaydash = this;
+    var newsId = $routeParams.id;
+
+    function getNewsById() {
+      path = 'getNewsById'+newsId;
+      NewsModel.getNewsById().then(function (result) {
+        displaydash.news = result.data;
+        console.log(displaydash.news);
+      });
+    }
+
+    getNewsById();
+  })
+
   .constant('ENDPOINT_URI', 'http://localhost:8080/api/news')
   .service('NewsModel', function ($http, ENDPOINT_URI) {
 
     var service = this;
-    var path = 'getNews/';
 
     function getUrl() {
       return ENDPOINT_URI + path;
@@ -27,10 +44,13 @@ angular.module('RESTService', [])
 
     service.all = function () {
       return $http.get(getUrl());
-    }_
+    };
+
+    service.getNewsById() = function(){
+      return $http.get(getUrl());
+    };
 
     service.create = function (news) {
       return $http.post(getUrl(), news);
-    }
-  })
-;
+    };
+  });
